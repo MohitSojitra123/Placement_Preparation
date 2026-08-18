71a501b73406bedb167da28e45584eee0b0edbd3
MohitSojitra123 <mohitsojitra599@gmail.com>
2026-08-18 14:25:56 +0530

    Added new Program

--- LeetCode_1_To_N_Problems/Programs/Same_Vowel_Contain_Question_1704_1_2.java
+++ LeetCode_1_To_N_Problems/Programs/Same_Vowel_Contain_Question_1704_1_2.java
@@ -27,7 +27,7 @@ class Solution {
     }
 }
 
-public class Same_Vowel_Contain_Question_1704_1 {
+public class Same_Vowel_Contain_Question_1704_1_2 {
     public static void main(String[] args) {
 
         Solution sol = new Solution();
