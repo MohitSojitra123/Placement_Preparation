2d4e96a87e3bd7078d02410e24dbc979425be2e3
MohitSojitra123 <mohitsojitra599@gmail.com>
2026-08-29 18:11:35 +0530

    Added New Progrmas

---
+++ Java_Programs/OOP_Advanced/Inner_Class.java
@@ -0,0 +1,19 @@
+interface Message {
+    void show();
+}
+
+public class Inner_Class {
+
+    public static void main(String[] args) {
+
+        Message m = new Message() {
+
+            public void show() {
+
+                System.out.println("Hello From Anonymous Class");
+            }
+        };
+
+        m.show();
+    }
+}
---
+++ Java_Programs/OOP_Advanced/Nested_Class_Without_Static.java
@@ -0,0 +1,19 @@
+// class A{
+//     class B{
+//         public void Print(){
+//             System.out.println("Nested Class Method : ");
+//         }
+//     }
+// }
+
+// public class Nested_Class_Without_Static {
+//     public static void main(String[] args) {
+        
+//           A a=new A();
+
+         
+
+
+
+//     }
+// }
